9d04fbc31cf37c4d017985727961ff3b541c4214
Leonard Lee <leonard.lee@praqma.com>
2018-10-08 08:19:14 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# jenkins-pipeline-library-abort-previous-builds
---
+++ vars/abortPreviousBuilds.groovy
@@ -0,0 +1,20 @@
+import hudson.model.Result
+import hudson.model.Run
+import jenkins.model.CauseOfInterruption.UserInterruption
+
+def call() {
+    // https://stackoverflow.com/a/49901413/4763512
+    Run previousBuild = currentBuild.rawBuild.getPreviousBuildInProgress()
+    while (previousBuild != null) {
+        if (previousBuild.isInProgress()) {
+            def executor = previousBuild.getExecutor()
+            if (executor != null) {
+                echo ">> Aborting older build #${previousBuild.number}."
+                executor.interrupt(Result.ABORTED, new UserInterruption(
+                    "Aborted by newer build #${currentBuild.number}."
+                ))
+            }
+        }
+        previousBuild = previousBuild.getPreviousBuildInProgress()
+    }
+}
